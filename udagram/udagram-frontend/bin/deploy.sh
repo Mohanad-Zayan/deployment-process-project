4987ff671d1706f1692911e1a875284087510514
Mohanad-zayan <mohanad.zayan2001@gmail.com>
2022-12-04 17:11:28 +0200

    first commit

---
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -0,0 +1,2 @@
+aws s3 cp --recursive --acl public-read ./www s3://front-udgram-12-4-2022/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://front-udgram-12-4-2022/
